aa5d926a3f7b76cc0bf28b1c980d21ba38d657c2
Rajeshgit <rajesqa87@gmail.com>
2018-07-02 09:50:01 +0530

    First commit

---
+++ Math_Proj/src/Basic/Add.java
@@ -0,0 +1,18 @@
+package Basic;
+
+public class Add 
+{
+	public static void main(String args[])
+	{
+		Add ob=new Add();
+		ob.add(2,3);
+	}
+	
+	public void add(int a,int b)
+	{
+		int c=a;
+		int d=b;
+		System.out.println("sum :"+(c+d));
+
+	}
+}
